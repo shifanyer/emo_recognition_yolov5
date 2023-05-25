029f39bc1a4c4bdec53d6760d99b5a672d6e8ddd
shifanyer <ivashka_shir@mail.ru>
2023-05-25 12:42:34 +0300

    add UI

--- diplom_wrapper/main_wrapper.py
+++ diplom_wrapper/main_wrapper.py
@@ -34,3 +34,35 @@ if __name__ == "__main__":
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     # print_args(vars(opt))
     custom_main(opt)
+
+
+def run_video_prediction():
+    weights = 'runs_new/exp64/weights/best.pt'
+    source = 'data/images/friends/Yarik/output1.mp4'
+    # source = 'data/images/video.mp4'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / weights, help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / source, help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[224], help='inference size h,w')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/predict-cls', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+    opt = parser.parse_args()
+    opt.save_txt = True
+    opt.view_img = False
+    opt.nosave = True
+    # opt.vid_stride = False
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    # print_args(vars(opt))
+    custom_main(opt)
--- diplom_wrapper/plot_draw.py
+++ diplom_wrapper/plot_draw.py
@@ -1,2 +1,49 @@
-l = [1, 2, 3, 4, 5, 6, 7]
-print(l[-10])
+# импортируем необходимые модули
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QMessageBox
+from diplom_wrapper.main_wrapper import run_video_prediction
+
+# создаем класс для нашего окна
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        # задаем заголовок и размеры окна
+        self.setWindowTitle("Сервис")
+        self.resize(300, 100)
+        # создаем три кнопки с разными надписями
+        self.button1 = QPushButton("Обработать видео с\n веб-камеры")
+        self.button2 = QPushButton("Обработать записанное видео")
+        self.button3 = QPushButton("Обработать файл")
+        # создаем горизонтальный лэйаут для размещения кнопок
+        self.layout = QHBoxLayout()
+        # добавляем кнопки в лэйаут
+        self.layout.addWidget(self.button1)
+        self.layout.addWidget(self.button2)
+        self.layout.addWidget(self.button3)
+        # устанавливаем лэйаут для окна
+        self.setLayout(self.layout)
+        # связываем функции с сигналами clicked кнопок
+        self.button1.clicked.connect(self.camera_prediction)
+        self.button2.clicked.connect(self.video_prediction)
+        self.button3.clicked.connect(self.data_prediction)
+
+    # определяем функции для обработки
+    def camera_prediction(self):
+        run_video_prediction()
+
+    def video_prediction(self):
+        run_video_prediction()
+
+    def data_prediction(self):
+        run_video_prediction()
+
+# создаем экземпляр приложения
+app = QApplication([])
+# создаем экземпляр окна
+window = Window()
+# показываем окно на экране
+window.show()
+# запускаем цикл обработки событий приложения
+app.exec_()
+
+
+
